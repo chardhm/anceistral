95139fa30fba40a4f1316f0fb26b3983c57e6209
chardhm <chych1317@gmail.com>
2022-03-07 16:21:02 -0600

    Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='hero'>
+            <div className="card bg-dark text-white border-0">
+                <img src="/assets/bg.jpg" className="card-img" alt="Backgrond" height="700px" />
+                    <div className="card-img-overlay d-flex flex-column">
+                        <div className="container">
+                           <h5 className="card-title display-3 fw-bolder mb-0">ACEITES MÁGICOS ARTESANALES</h5>
+                           <p className="card-text lead fs-2">
+                               CHECA TODOS NUESTROS PRODUCTOS
+                           </p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 export const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
-                <div class="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+                <div className="container">
                   <img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-olive-oil-healthy-food-and-vegan-justicon-flat-justicon.png" width="26" alt="" />
-                    <a class="navbar-brand fw-bold fs-4" href="#">Aceistral</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <a className="navbar-brand fw-bold fs-4" href="#">Aceistral</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Products</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Products</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
                             </li>
                         </ul>
                         <div className="buttons">
